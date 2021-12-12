babc28ed2e51498405d02bcc0109324af193be5f
ywang43 <james.frosty3150102@gmail.com>
2021-12-12 13:50:07 +0800

    strncpyTest issue -

---
+++ cpp/strncpyTest.cpp
@@ -0,0 +1,17 @@
+﻿
+//#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char m[] = "hello";
+    char m2[20];
+    char m3[30];
+
+    strcpy(m2, m);
+    printf("%s\n",m2);
+
+    strncpy(m3, m, 20 - 1);
+}
+
